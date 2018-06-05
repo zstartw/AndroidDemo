6c85350a46d0c77f74658f69ebcfa5ea515b37d3
zhengwu <zhengwu@globalegrow.com>
2018-06-05 11:02:30 +0800

    线程相关demo

--- threadsample/src/main/java/com/betterzw/threadsample/MainActivity.java
+++ threadsample/src/main/java/com/betterzw/threadsample/MainActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.util.HashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+
+
+    String show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,21 +24,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-       /* HashMap<String, String> test = new HashMap<>();
-        test.put("1", "rock");
-        test.put("2", "zw");
-        test.put("1", "111");
+//        ExecutorService service = Executors.newFixedThreadPool(2);
+       /* ExecutorService service = Executors.newCachedThreadPool();
 
-        for (String key : test.keySet()){
-            Log.d(TAG, "name:"+key);
-            Log.d(TAG, "value:"+test.get(key));
+        Thread t1 = new MyThread1();
+        Thread t2 = new MyThread2();
+        Thread t3 = new MyThread3();
+        Thread t4 = new MyThread4();
+        Thread t5 = new MyThread4();
+
+
+        service.execute(t1);
+        service.execute(t2);
+        service.execute(t3);
+        service.execute(t4);
+        service.execute(t5);*/
+
+        StringBuilder rowStr = new StringBuilder();
+
+        String test = "test";
+
+        Thread t5 = new MyThread4();
+
+        show = new String("title");
+
+    }
+
+    class MyThread1 extends Thread {
+        @Override
+        public void run() {
+            Log.d(TAG, "run: 1111");
+            Log.d(TAG, "run: "+Thread.currentThread().getName());
         }
+    }
 
-        Log.d(TAG, "onCreate: ==="+new String("1").hashCode());
-        Log.d(TAG, "onCreate: ==="+new String("12").hashCode());
-        Log.d(TAG, "onCreate: ==="+new String("13").hashCode());
-        Log.d(TAG, "onCreate: ==="+(new String("1111111111").hashCode() & 1));
+    class MyThread2 extends Thread {
+        @Override
+        public void run() {
+            Log.d(TAG, "run: 2222");
+            Log.d(TAG, "run: "+Thread.currentThread().getName());
+        }
+    }
 
-        Log.d(TAG, "onCreate==========: "+new Object().hashCode());*/
+    class MyThread3 extends Thread {
+        @Override
+        public void run() {
+            Log.d(TAG, "run: 333");
+            Log.d(TAG, "run: "+Thread.currentThread().getName());
+        }
+    }
+
+    class MyThread4 extends Thread {
+        @Override
+        public void run() {
+            Log.d(TAG, "run: 444");
+            Log.d(TAG, "run: "+Thread.currentThread().getName());
+        }
     }
 }
